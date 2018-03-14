c4e6f67049658b366f20984d58e3992ba7c73334
Anton Tkach <dekerter@gmail.com>
2018-03-14 13:29:13 +0200

    Fixed naming logic
    Work in progress. Needs to be reviewed and finished

--- src/rental/RentalPriceCalculator.java
+++ src/rental/RentalPriceCalculator.java
@@ -1,43 +1,46 @@
 package rental;
 
 public class RentalPriceCalculator {
-	
-	// age - age of driver
-	// licence - number of full years person holds driving licence
-	// clazz - class of the car from 1 (smallest) to 5 (largest) that person wishes to rent
-	// acc - has s/he caused any accidents within last year
-	// acc2 - has s/he participated (but not caused) in any accidents within last year
-	// season - if it is high season or not
-	public double price(int age, int licence, int clazz, boolean acc, boolean acc2, boolean season) {
-		
-		if (age < 18) {
-			throw new IllegalArgumentException("Driver too young - cannot quote the price");
-		}
-		if (age <= 21 && clazz > 2) {
-			throw new UnsupportedOperationException("Drivers 21 y/o or less can only rent Class 1 vehicles");
-		}
-		
-		double rentalprice = age;
-		
-		if (clazz >=4 && age <= 25 && season != false) {
-			rentalprice = rentalprice * 2;
-		}
-		
-		if (licence < 1) {
-			throw new IllegalArgumentException("Driver must hold driving licence at least for one year. Can not rent a car!");
-		}
-		
-		if (licence < 3) {
-			rentalprice = rentalprice * 1.3;
-		}
-		
-		if (acc == true && age < 30) {
-			rentalprice += 15;
-		}
 
-		if (rentalprice > 1000) {
-			return 1000.00;
-		}
-		return rentalprice;
-	}
+    // age - age of driver
+    // yearsLicenceHeld - number of full years person holds driving licence
+    // carClass - class of the car from 1 (smallest) to 5 (largest) that person wishes to rent
+    // isAccidentCaused - has s/he caused any accidents within last year
+    // isAaccidentInvolved - has s/he participated (but not caused) in any accidents within last year
+    // isSeasonHigh - if it is high season or not
+    private static final int MIN_AGE = 18;
+    private static final int MIDDLE_AGE = 21;
+
+    public double CalculateRentalPrice(int age, int yearsLicenceHeld, int carClass, boolean isAccidentCaused, boolean isSeasonHigh) {
+
+        if (age < MIN_AGE) {
+            throw new IllegalArgumentException("Driver too young - cannot quote the price");
+        }
+        if (age <= MIDDLE_AGE && carClass > 2) {
+            throw new UnsupportedOperationException("Drivers 21 y/o or less can only rent Class 1 vehicles");
+        }
+
+        double rentalPrice = age;
+
+        if (carClass >= 4 && age <= 25 && isSeasonHigh != false) {
+            rentalPrice = rentalPrice * 2;
+        }
+
+        if (yearsLicenceHeld < 1) {
+            throw new IllegalArgumentException("Driver must hold driving licence at least for one year. Can not rent a car!");
+        }
+
+        if (yearsLicenceHeld < 3) {
+            rentalPrice = rentalPrice * 1.3;
+        }
+
+        if (isAccidentCaused == true && age < 30) {
+            rentalPrice += 15;
+        }
+
+        if (rentalPrice > 1000) {
+            return 1000.00;
+        }
+        return rentalPrice;
+    }
 }
--- test/rental/RentalPriceCalculatorTest.java
+++ test/rental/RentalPriceCalculatorTest.java
@@ -1,5 +1,6 @@
 package rental;
 
+import static com.sun.xml.internal.ws.dump.LoggingDumpTube.Position.Before;
 import static org.junit.Assert.fail;
 
 import org.junit.Before;
